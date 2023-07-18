50e2842e4818434e7f8b2b4b46aa1a893cb9bc0a
Raj R <58966550+rajrku@users.noreply.github.com>
2023-07-18 11:27:58 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,5 +15,14 @@ sudo su
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.6.linux-amd64.tar.gz
 
 export PATH=$PATH:/usr/local/go/bin
-go version
+/usr/local/go/bin/go version
 
+export CGO_ENABLED=1
+sudo apt install -y zip bison build-essential cmake flex git libedit-dev \
+  libllvm12 llvm-12-dev libclang-12-dev python zlib1g-dev libelf-dev libfl-dev python3-setuptools \
+  liblzma-dev arping netperf iperf
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4052245BD4284CDD
+echo "deb https://repo.iovisor.org/apt/$(lsb_release -cs) $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/iovisor.list
+sudo apt-get update
+sudo apt-get install bcc-tools libbcc-examples linux-headers-$(uname -r)
