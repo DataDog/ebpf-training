7abda6ecaa952024448ff66e17b050a4743c9272
Pansy Ng <37123569+pansyng@users.noreply.github.com>
2023-07-19 10:38:46 -0700

    Update setup_python_ebpf.sh

--- setup_python_ebpf.sh
+++ setup_python_ebpf.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4052245BD4284CDD
-echo "deb https://repo.iovisor.org/apt/$(lsb_release -cs) $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/iovisor.list
-sudo apt-get update
-sudo apt-get install bcc-tools libbcc-examples linux-headers-$(uname -r)
-
 sudo apt install -y python3-pip
 sudo pip3 install dnslib
 sudo python3 python_ebpf/final_code_eBPF_dns.py
