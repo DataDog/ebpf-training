4cde121d79a2fac98468b4bf5a848c6c426264a5
Tommaso Pieroncini <tommasopieroncini@github.com>
2023-07-19 14:44:57 +0000

    change setup script to use local copy of python program

--- setup_python_ebpf.sh
+++ setup_python_ebpf.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-wget https://gist.githubusercontent.com/oghie/b4e3accf1f87afcb939f884723e2b462/raw/fe60e6b66135640ea39c878589fb092b6eb838a1/final_code_eBPF_dns.py
 sudo apt install -y python3-pip
 sudo pip3 install dnslib
-sudo python3 final_code_eBPF_dns.py
+sudo python3 python_ebpf/final_code_eBPF_dns.py
